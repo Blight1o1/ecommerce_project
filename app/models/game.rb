d193778a9e5f7ec0c3cb5095180c1285281b4454
Jordan <jblight34@rrc.ca>
2021-11-17 13:15:16 -0600

    Added links to genre and platfrom to game page.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
     has_many :game_genres
     has_many :game_platforms
-    has_many :genres, :through :game_genres
-    has_many :platfroms, :through: :game_platforms
+    has_many :genres, through: :game_genres
+    has_many :platforms, through: :game_platforms
 end
