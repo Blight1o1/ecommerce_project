9579c03d6aada0128aa7dafaf3147fbb29ca0707
Jordan <jblight34@rrc.ca>
2021-11-18 10:50:30 -0600

    Added About + Contact controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'about/show'
+  get 'contact/show'
   resources :games
   resources :genres
   resources :platforms
+
+  get "/contact",         to: "contact#show"
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
