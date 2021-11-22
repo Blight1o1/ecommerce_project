c12fb073cd30842e21c590bcc70ef6df448ad81b
Jordan <jblight34@rrc.ca>
2021-11-22 15:57:06 -0600

    Creating basic accounts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    #before_action :authenticate_user!
+    #has_secure_password
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+
 end
